98f0402755af436aa655a47df3ce160d0fae47fe
Shruthi MV <mv.shruthi10@gmail.com>
2020-06-17 14:24:25 +0530

    Firstss Commit

---
+++ src/test/java/com/testyantra/scripts/AccountTest.java
@@ -0,0 +1,14 @@
+package com.testyantra.scripts;
+
+import org.testng.annotations.Test;
+
+public class AccountTest {
+	
+	@Test
+	public void test1() {
+		
+		System.out.println("Test1 method executed");
+		
+	}
+
+}
